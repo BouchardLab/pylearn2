003d852377550b92c0443bb9de4f18aa92c22358
JesseLivezey <jesse.livezey@gmail.com>
2014-12-02 12:41:39 -0800

    fixed labels bug in dataset

---
+++ pylearn2/datasets/ecog.py
@@ -0,0 +1,77 @@
+"""
+ECoG dataset.
+"""
+__authors__ = "Jesse Livezey"
+
+import numpy as N
+np = N
+import h5py
+from theano.compat.six.moves import xrange
+from pylearn2.datasets import dense_design_matrix
+from pylearn2.datasets import control
+from pylearn2.datasets import cache
+from pylearn2.utils import serial
+from pylearn2.utils.rng import make_np_rng
+
+
+class ECoG(dense_design_matrix.DenseDesignMatrix):
+    """
+    ECoG dataset
+
+    Parameters
+    ----------
+    filename : str
+        Filename for data.
+    which_set : str
+        'train' or 'valid'
+    frac_train : float
+        Fraction of data for training. Remaining data is for validation.
+    center : bool
+        If True, preprocess so that data has zero mean.
+    """
+
+    def __init__(self, filename, which_set, frac_train, center=False):
+        self.args = locals()
+
+        if which_set not in ['train', 'valid']:
+            raise ValueError(
+                'Unrecognized which_set value "%s".' % (which_set,) +
+                '". Valid values are ["train","valid"].')
+        with h5py.File(filename,'r') as f:
+            X = f['X'].value
+            y = f['y'].value
+        rng = make_np_rng()
+        n_examples = X.shape[0]
+        order = rng.permutation(n_examples)
+        n_train = int(n_examples*frac_train)
+        X_train = X[order[:n_train]]
+        X_valid = X[order[n_train:]]
+        y_train = y[order[:n_train]]
+        y_valid = y[order[n_train:]]
+        if which_set == 'train':
+            topo_view = X_train[...,np.newaxis]
+            y_final = y_train
+        else:
+            topo_view = X_valid[...,np.newaxis]
+            y_final = y_valid
+        if center:
+            topo_view = topo_view-X_train[...,np.newaxis].mean(0)
+
+        super(ECoG, self).__init__(topo_view=topo_view.astype('float32'),
+                                    y=y_final.astype('float32'),
+                                    axes=('b',0,1,'c'))
+
+        assert not N.any(N.isnan(self.X))
+        assert not N.any(N.isnan(self.y))
+
+    def get_valid_set(self):
+        """
+        .. todo::
+
+            WRITEME
+        """
+        args = {}
+        args.update(self.args)
+        del args['self']
+        args['which_set'] = 'valid'
+        return ECoG(**args)
