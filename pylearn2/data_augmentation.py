f539cb9e687247a6bb550c230049d2fae66c59cb
JesseLivezey <jesse.livezey@gmail.com>
2015-05-05 14:56:42 -0700

    changed to cpu augmentation

--- pylearn2/data_augmentation.py
+++ pylearn2/data_augmentation.py
@@ -5,11 +5,12 @@ import theano
 
 class ScaleAugmentation(Block):
 
-    def __init__(self, space, seed=20150111, mean=1., std=.05):
+    def __init__(self, space, seed=20150111, mean=1., std=.05, cpu_only=True):
         self.rng = make_theano_rng(seed, which_method=['normal'])
         self.mean = mean
         self.std = std
         self.space = space
+        self.cpu_only = cpu_only
         super(ScaleAugmentation, self).__init__()
 
     def create_theano_function(self):
@@ -22,7 +23,11 @@ class ScaleAugmentation(Block):
             scale = self.rng.normal(size=[X.shape[0]], avg=self.mean, std=self.std)
             scale = scale.dimshuffle(0,*arg)
             out = X*scale
-            return theano.function([X], out)
+            if self.cpu_only:
+                mode = get_default_mode().excluding('gpu')
+            else:
+                mode = get_default_mode()
+            return theano.function([X], out, mode=mode)
 
     def perform(self, X):
         f = self.create_theano_function()
