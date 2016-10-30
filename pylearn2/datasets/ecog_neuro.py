7fc9cee2b8606a777deb127125403c9b5800cc20
JesseLivezey <jesse.livezey@gmail.com>
2016-10-29 21:29:46 -0700

    dataset changes

--- pylearn2/datasets/ecog_neuro.py
+++ pylearn2/datasets/ecog_neuro.py
@@ -16,12 +16,11 @@ from pylearn2.utils import serial
 from pylearn2.utils.rng import make_np_rng
 
 
-def complex_pca_function(X, final_dim):
+def complex_pca_function(X, final_dim=2000):
     n_samples, n_features = X.shape
     u, s, v = np.linalg.svd(X, full_matrices=False)
     X_pca = u[:, :final_dim] * s[np.newaxis, :final_dim]
     K = v[:final_dim].conj().T
-    print v.shape, K.shape
 
     class PCA(object):
         def __init__(self, K):
@@ -70,7 +69,8 @@ class ECoG(dense_design_matrix.DenseDesignMatrix):
     """
 
     def __init__(self, subject, bands, data_types,
-                 which_set, fold=0, seed=20161022, center=True,
+                 which_set, fold=0,
+                 seed=20161022, center=True,
                  move = .1, level_classes=True,
                  consonant_prediction=False,
                  vowel_prediction=False,
@@ -83,7 +83,7 @@ class ECoG(dense_design_matrix.DenseDesignMatrix):
                  consonant_labels=19,
                  min_cvs=10,
                  condense=True,
-                 total_dim=1000):
+                 total_dim=2000):
         self.args = locals()
 
         possible_subjects = ['EC2', 'EC9', 'GP31', 'GP33']
@@ -116,7 +116,7 @@ class ECoG(dense_design_matrix.DenseDesignMatrix):
         assert all(b in possible_bands for b in bands)
 
         if subject == 'EC2':
-            filename = 'EC2_blocks_1_8_9_15_76_89_105_CV_neuro_18_align_window_-05_to_079_between_data_nobaseline.h5'
+            filename = 'EC2_blocks_1_8_9_15_76_89_105_CV_narrow_neuro_18_align_window_-05_to_079_between_data_nobaseline.h5'
         else:
             raise ValueError
         filename = os.path.join('${PYLEARN2_DATA_PATH}/ecog/hdf5', filename)
@@ -127,27 +127,37 @@ class ECoG(dense_design_matrix.DenseDesignMatrix):
         rng = np.random.RandomState(seed)
 
         filename = serial.preprocess(filename)
+        stored = False
+
         with h5py.File(filename,'r') as f:
-            Xs = [f['X{}'.format(b)].value for b in bands]
-            y = f['y'].value.astype(int)
-            if two_headed:
-                assert not consonant_prediction
-                assert not vowel_prediction
-                y_consonant = f['y_consonant'].value
-                y_vowel = f['y_vowel'].value
-            elif consonant_prediction:
-                assert not vowel_prediction
-                y_consonant = f['y_consonant'].value
-            elif vowel_prediction:
-                assert not consonant_prediction
-                y_vowel = f['y_vowel'].value
-            if which_set == 'augment':
-                X_aug = f['X_aug'].value
-                assert X_aug.shape[1] == y.shape[0]
-                tile_len = X_aug.shape[0]
-                y_aug = y[np.newaxis,...]
-                y_aug = np.tile(y, (tile_len, 1))
-            
+            try:
+                X_train_tmp = [f['fold{}'.format(fold)][b]['train']['X'].value for b in bands]
+                X_valid_tmp = [f['fold{}'.format(fold)][b]['valid']['X'].value for b in bands]
+                X_test_tmp = [f['fold{}'.format(fold)][b]['test']['X'].value for b in bands]
+                y_train = f['fold{}'.format(fold)][bands[0]]['train']['y'].value.astype(int)
+                y_valid = f['fold{}'.format(fold)][bands[0]]['valid']['y'].value.astype(int)
+                y_test = f['fold{}'.format(fold)][bands[0]]['test']['y'].value.astype(int)
+                stored = True
+            except KeyError:
+                Xs = [f['X{}'.format(b)].value for b in bands]
+                y = f['y'].value.astype(int)
+                if two_headed:
+                    assert not consonant_prediction
+                    assert not vowel_prediction
+                    y_consonant = f['y_consonant'].value
+                    y_vowel = f['y_vowel'].value
+                elif consonant_prediction:
+                    assert not vowel_prediction
+                    y_consonant = f['y_consonant'].value
+                elif vowel_prediction:
+                    assert not consonant_prediction
+                    y_vowel = f['y_vowel'].value
+                if which_set == 'augment':
+                    X_aug = f['X_aug'].value
+                    assert X_aug.shape[1] == y.shape[0]
+                    tile_len = X_aug.shape[0]
+                    y_aug = y[np.newaxis,...]
+                    y_aug = np.tile(y, (tile_len, 1))
 
         def split_indices(indices, frac_train, min_cvs):
             """
@@ -215,126 +225,131 @@ class ECoG(dense_design_matrix.DenseDesignMatrix):
             assert len(union)-1 == max_val
             assert len(tr)+len(va)+len(te)+len(ex) == len(union)
 
-        n_examples = Xs[0].shape[0]
-        assert all(n_examples == X.shape[0] for X in Xs)
-        self.present_cvs = np.zeros(y_labels, dtype=int)
-        if level_classes:
-            n_classes = y_labels
-            class_indices = {}
-            for ii in xrange(n_classes):
-                class_indices[str(ii)] = np.nonzero(y == ii)[0].tolist()
-            total = 0
-            for indices in class_indices.values():
-                total += len(indices)
-            assert total == y.shape[0]
-            train_idx = []
-            valid_idx = []
-            test_idx = []
-            extra_idx = []
-            for ii, key in enumerate(sorted(class_indices.keys())):
-                tr, va, te, ex = split_indices(class_indices[key], frac_train, min_cvs)
-                if len(tr) > 0:
-                    self.present_cvs[ii] = 1
-                train_idx += tr
-                valid_idx += va
-                test_idx += te
-                extra_idx += ex
-        else:
-            indices = range(n_examples)
-            train_idx, valid_idx, test_idx, extra_idx = split_indices(indices, frac_train, min_cvs)
+        if not stored:
 
-        check_indices(train_idx, valid_idx, test_idx, extra_idx)
-
-        self.indices = (train_idx, valid_idx, test_idx, extra_idx)
-
-
-        n_classes = y_labels
-        if two_headed:
-            y = np.hstack((y_consonant, y_vowel))
-            raise NotImplementedError
-        elif consonant_prediction:
-            assert not vowel_prediction
-            y = y_consonant
-            n_classes = consonant_labels
-        elif vowel_prediction:
-            assert not consonant_prediction
-            y = y_vowel
-            n_classes = vowel_labels
-
-        if randomize_labels:
-            if which_set == 'augment':
-                raise NotImplementedError
-            in_idx = np.concatenate((train_idx, valid_idx, test_idx))
-            order = rng.permutation(in_idx.shape[0])
-            for X in Xs:
-                X[in_idx] = X[in_idx][order]
-
-        X_train = [X[train_idx] for X in Xs]
-        X_valid = [X[valid_idx] for X in Xs]
-        X_test = [X[test_idx] for X in Xs]
-        y_train = y[train_idx]
-        y_valid = y[valid_idx]
-        y_test = y[test_idx]
-
-        X_train_tmp = []
-        X_valid_tmp = []
-        X_test_tmp = []
-        for ii, (X, dim, dt) in enumerate(zip(X_train, dims, data_types)):
-            if dt == 'complex':
-                dim = dim //2
-                def dt_func(X):
-                    return np.hstack((X.real, X.imag))
-            elif dt == 'phase':
-                def dt_func(X):
-                    return np.angle(X)
-            elif dt == 'amplitude':
-                def dt_func(X):
-                    return abs(X)
+            n_examples = Xs[0].shape[0]
+            assert all(n_examples == X.shape[0] for X in Xs)
+            self.present_cvs = np.zeros(y_labels, dtype=int)
+            if level_classes:
+                n_classes = y_labels
+                class_indices = {}
+                for ii in xrange(n_classes):
+                    class_indices[str(ii)] = np.nonzero(y == ii)[0].tolist()
+                total = 0
+                for indices in class_indices.values():
+                    total += len(indices)
+                assert total == y.shape[0]
+                train_idx = []
+                valid_idx = []
+                test_idx = []
+                extra_idx = []
+                for ii, key in enumerate(sorted(class_indices.keys())):
+                    tr, va, te, ex = split_indices(class_indices[key], frac_train, min_cvs)
+                    if len(tr) > 0:
+                        self.present_cvs[ii] = 1
+                    train_idx += tr
+                    valid_idx += va
+                    test_idx += te
+                    extra_idx += ex
             else:
-                raise ValueError
-            n_ex = X.shape[0]
-            X_pca, pca = complex_pca_function(X.reshape(n_ex, -1), dim)
-            X_train_tmp.append(dt_func(X_pca))
-            print X_train_tmp[-1].shape
-            n_ex = X_valid[ii].shape[0]
-            X_valid_tmp.append(dt_func(pca.transform(X_valid[ii].reshape(n_ex, -1))))
-            print X_valid_tmp[-1].shape
-            n_ex = X_test[ii].shape[0]
-            X_test_tmp.append(dt_func(pca.transform(X_test[ii].reshape(n_ex, -1))))
-            print X_train_tmp[-1].shape
-        X_train = X_train_tmp
-        X_valid = X_valid_tmp
-        X_test = X_test_tmp
-        X_train = np.hstack(X_train)
-        X_valid = np.hstack(X_valid)
-        X_test = np.hstack(X_test)
+                indices = range(n_examples)
+                train_idx, valid_idx, test_idx, extra_idx = split_indices(indices, frac_train, min_cvs)
+
+            check_indices(train_idx, valid_idx, test_idx, extra_idx)
+
+            self.indices = (train_idx, valid_idx, test_idx, extra_idx)
+
+
+            n_classes = y_labels
+            if two_headed:
+                y = np.hstack((y_consonant, y_vowel))
+                raise NotImplementedError
+            elif consonant_prediction:
+                assert not vowel_prediction
+                y = y_consonant
+                n_classes = consonant_labels
+            elif vowel_prediction:
+                assert not consonant_prediction
+                y = y_vowel
+                n_classes = vowel_labels
+
+            if randomize_labels:
+                if which_set == 'augment':
+                    raise NotImplementedError
+                in_idx = np.concatenate((train_idx, valid_idx, test_idx))
+                order = rng.permutation(in_idx.shape[0])
+                for X in Xs:
+                    X[in_idx] = X[in_idx][order]
+
+            X_train = [X[train_idx] for X in Xs]
+            X_valid = [X[valid_idx] for X in Xs]
+            X_test = [X[test_idx] for X in Xs]
+            y_train = y[train_idx]
+            y_valid = y[valid_idx]
+            y_test = y[test_idx]
+
+            X_train_tmp = []
+            X_valid_tmp = []
+            X_test_tmp = []
+            for ii, (X, dim, dt) in enumerate(zip(X_train, dims, data_types)):
+                if dt == 'complex':
+                    dim = dim //2
+                    def dt_func(X):
+                        return np.hstack((X.real, X.imag))
+                elif dt == 'phase':
+                    def dt_func(X):
+                        return np.angle(X)
+                elif dt == 'amplitude':
+                    def dt_func(X):
+                        return abs(X)
+                else:
+                    raise ValueError
+                n_ex = X.shape[0]
+                mean = X.reshape(n_ex, -1).mean(axis=0, keepdims=True)
+                X_pca, pca = complex_pca_function(X.reshape(n_ex, -1)-mean)
+                X_train_tmp.append(dt_func(X_pca))
+                n_ex = X_valid[ii].shape[0]
+                X_valid_tmp.append(dt_func(pca.transform(X_valid[ii].reshape(n_ex,-1)-mean)))
+                n_ex = X_test[ii].shape[0]
+                X_test_tmp.append(dt_func(pca.transform(X_test[ii].reshape(n_ex,-1)-mean)))
+                with h5py.File(filename) as f:
+                    for e in ['train', 'valid', 'test']:
+                        for d in ['X', 'y']:
+                            try:
+                                del f['fold{}'.format(fold)][bands[ii]][e][d]
+                            except KeyError:
+                                pass
+                        try:
+                            del f['fold{}'.format(fold)][bands[ii]][e]
+                        except KeyError:
+                            pass
+                    try:
+                        g0 = f.create_group('fold{}'.format(fold))
+                    except ValueError:
+                        g0 = f['fold{}'.format(fold)]
+                    try:
+                        g1 = g0.create_group(bands[ii])
+                    except ValueError:
+                        g1 = g0[bands[ii]]
+
+                    g2 = g1.create_group('train')
+                    g2.create_dataset('X', data=X_train_tmp[-1])
+                    g2.create_dataset('y', data=y_train)
+
+                    g2 = g1.create_group('valid')
+                    g2.create_dataset('X', data=X_valid_tmp[-1])
+                    g2.create_dataset('y', data=y_valid)
+
+                    g2 = g1.create_group('test')
+                    g2.create_dataset('X', data=X_test_tmp[-1])
+                    g2.create_dataset('y', data=y_test)
+
+        X_train = np.hstack([X[:, :d] for X, d in zip(X_train_tmp, dims)])
+        X_valid = np.hstack([X[:, :d] for X, d in zip(X_valid_tmp, dims)])
+        X_test = np.hstack([X[:, :d] for X, d in zip(X_test_tmp, dims)])
 
         self.train_mean = X_train.mean(axis=0, keepdims=True)
         print X_train.shape, X_valid.shape, X_test.shape, self.train_mean.shape
-
-        if which_set == 'augment':
-            raise NotImplementedError
-            img_shape = X_train.shape[1:]
-            y_shape = y_train.shape[1:]
-            if pm_aug_range is not None:
-                possible_range = X_aug.shape[0]
-                assert possible_range % 2 == 1
-                assert possible_range >= 2*pm_aug_range+1
-                pm_possible = int(np.round(possible_range-1)/2.)
-                idxs = slice(pm_possible-pm_aug_range, possible_range-(pm_possible-pm_aug_range))
-                X_aug = X_aug[idxs]
-                y_aug = y_aug[idxs]
-            X_aug = X_aug[:,train_idx]
-            X_aug = X_aug.reshape(-1,*img_shape)
-            y_aug = y_aug[:,train_idx]
-            y_aug = y_aug.reshape(-1,*y_shape)
-            X_aug = np.concatenate((X_train, X_aug))
-            y_aug = np.concatenate((y_train, y_aug))
-            order = rng.permutation(X_aug.shape[0])
-            X_train = X_aug[order]
-            y_train = y_aug[order]
-            del X_aug
-            del y_aug
 
         if (which_set == 'train') or (which_set == 'augment'):
             topo_view = X_train
@@ -369,7 +384,6 @@ class ECoG(dense_design_matrix.DenseDesignMatrix):
                     curr_idx += 1
             n_classes = curr_idx
             y_final = y_condensed
-
 
         super(ECoG, self).__init__(topo_view=topo_view.astype('float32'),
                                     y=y_final[:, np.newaxis],
