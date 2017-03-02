8fdf8d33e7b93c0659b81a615c5dc69f2ee3576a
JesseLivezey <jesse.livezey@gmail.com>
2017-03-02 14:03:45 -0800

    plot change

--- pylearn2/scripts/ecog/make_state_correlation_plots.py
+++ pylearn2/scripts/ecog/make_state_correlation_plots.py
@@ -29,7 +29,11 @@ colors = ['green', 'black', 'red']
 
 def load_data(path):
     data = np.load(path)
-    return data['ccp'], data['ccm'], data['ccv'], data['ccmjar']
+    try:
+        ccmjar = data['ccmjar']
+    except KeyError:
+        ccmjar = None
+    return data['ccp'], data['ccm'], data['ccv'], ccmjar
 
 rp = []
 rm = []
@@ -56,6 +60,7 @@ for fname in linear_files:
     lm.extend(data[1])
     lv.extend(data[2])
     """
+
 for fname in deep_files:
     path = os.path.join(folder, fname)
     data = load_data(path)
@@ -65,27 +70,25 @@ for fname in deep_files:
     dmjar.extend(data[3])
 
 
-"""
 box_params = {'notch': False,
               'sym': '',
               'vert': False,
               'whis': 0,
-              'labels': ('',
-                         'Vowel',
-                         '',
+              'labels': ('Vowel',
                          'Manner',
-                         '',
-                         'Place'),
-              'positions': [2-.375, 2+.375, 4-.375, 4+.375, 6-.375, 6+.375],
+                         'Place',
+                         'Maj. Art.'),
+              'positions': [2-.375, 3-.375, 4-.375, 5-.375],
               'medianprops': {'color': 'black', 'linewidth': 2},
               'boxprops': {'color': 'black', 'linewidth': 2}}
 
 #data = [rv, lv, dv, rm, lm, dm, rp, lp, dp]
-data = [lv, dv, lm, dm, lp, dp]
+data = [dv, dm, dp, dmjar]
 f = plt.figure(figsize=(15, 8))
 bp = plt.boxplot(data, **box_params)
 for ii in range(len(bp['boxes'])):
     c = colors[ii % (len(colors)-1)+1]
+    c = 'k'
     plt.setp(bp['boxes'][ii], color=c)
     plt.setp(bp['caps'][2*ii], color=c)
     plt.setp(bp['caps'][2*ii+1], color=c)
@@ -94,14 +97,14 @@ for ii in range(len(bp['boxes'])):
     plt.setp(bp['medians'][ii], color=c)
 plt.plot(0,0, '-', c='red', label='Deep')
 plt.plot(0,0, '-', c='black', label='Linear')
-plt.xlim([-.06, .45])
+plt.xlim([-.06, .65])
 #plt.plot(0,0, '-', c='red', label='Neural Data')
-plt.legend(loc='best', prop={'size': 20})
+#plt.legend(loc='best', prop={'size': 20})
 plt.xlabel('Correlation Coefficient')
 plt.savefig('state_correlation.png')
 plt.savefig('state_correlation.pdf')
 
-
+"""
 positions = [1-.375, 1+.375, 3-.375, 3+.375, 5-.375, 5+.375]
 box_params = {'vert': False,
               'showmedians': True,
@@ -142,6 +145,7 @@ plt.savefig('state_correlation_violin.pdf')
 """
 
 
+"""
 positions = [1, 2, 3, 4]
 box_params = {'vert': False,
               'showmedians': True,
@@ -157,9 +161,10 @@ vp['cmins'].set_color(['red', 'red', 'red', 'red'])
 vp['cmins'].set_linewidths(2)
 vp['cmaxes'].set_color(['red', 'red', 'red', 'red'])
 vp['cmaxes'].set_linewidths(2)
-"""
 from IPython import embed
 embed()
+"""
+
 """
 for ii in range(len(vp['bodies'])):
     c = colors[ii % (len(colors)-1)+1]
@@ -183,3 +188,4 @@ plt.legend(loc='lower right', prop={'size': 20})
 plt.xlabel('Correlation Coefficient')
 plt.savefig('state_correlation_violin.png')
 plt.savefig('state_correlation_violin.pdf')
+"""
